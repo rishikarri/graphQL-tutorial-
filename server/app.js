307d900ec72d5930e08d2e9b18977a33c6d6bcd6
Rishi Karri <rishi.karri@turner.com>
2019-06-21 13:47:46 -0400

    connected to database

--- server/app.js
+++ server/app.js
@@ -2,7 +2,15 @@ const express = require('./node_modules/express');
 const graphqlHTTP = require('./node_modules/express-graphql/dist');
 const app = express();
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const config = require('../config.json');
 
+const connectionString = `mongodb+srv://rishikarri:${config.rishikarri}@graphql-tutorial-2ubrf.mongodb.net/test?retryWrites=true&w=majority`
+mongoose.connect(connectionString, { useNewUrlParser: true });
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
     schema,
